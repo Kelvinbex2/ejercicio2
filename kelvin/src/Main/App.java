0d93975bca715922329705305f725e59a4265315
kelvinbex2 <obamedo20@gmail.com>
2024-04-03 11:48:21 +0200

    ss

--- kelvin/src/Main/App.java
+++ kelvin/src/Main/App.java
@@ -1,7 +1,29 @@
 package Main;
 
+import Model.Asesores;
+import Model.Cajero;
+import Model.Cola;
+import Model.Empleado;
+import Model.Gerente;
+import Model.Tarea;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Crear cola de tareas
+        Cola cola = new Cola();
+        // Agregar empleados a la cola de tareas
+
+        Tarea tarea1=new Tarea(null, );
+
+        // Crear empleados
+        Empleado asesores = new Asesores("Asesores", 1, null);
+        Empleado gerente = new Gerente("Gerente", 2, null);
+        Empleado cajero = new Cajero("Cajero", 3, null);
+        
+        // Agregar empleados a la cola de tareas
+        cola.agregar(asesores);
+        cola.agregarTarea(gerente);
+        cola.agregar(cajero);
+        
     }
 }
